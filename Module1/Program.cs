9b7b22cf2d22a2d7e3e5f5dfcd38c3a10fe13436
cezarr46 <sezay.ismail@gmail.com>
2017-07-07 13:29:03 +0300

    Add files via upload

---
+++ Module1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Module1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName, lastName, addressLine1, addressLine2, city, state, country;
+            DateTime birthdate;
+            int zip;
+
+            firstName = "Sezay";
+            lastName = "Ismail";
+            addressLine1 = "Studentski grad";
+            addressLine2 = "blok 2";
+            city = "Sofia";
+            state = "Sofia";
+            country = "Bulgaria";
+            birthdate = new DateTime(1994, 10, 02);
+            zip = 1000;
+
+            Console.WriteLine($"First Name: {firstName},\nLast Name: {lastName},\nBirthdate: {birthdate},\nAddress Line 1: {addressLine1},\nAddress Line 2: {addressLine2},\nCity: {city},\nState/Province: {state},\nZip/Postal: {zip},\nCountry: {country}");
+
+        }
+    }
+}
