2f80adba277161377c581c3c533a3b3fae5408eb
cezarr46 <sezay.ismail@gmail.com>
2017-07-13 11:03:00 +0300

    Add files via upload

---
+++ Module9/Mod_9_Homework/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module9
+{
+    class Student
+    {
+        private string firstName;
+        private string lastName;
+        private string city;
+        internal static List<Student> students = new List<Student>();
+        internal static int count = 0;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+    }
+}
