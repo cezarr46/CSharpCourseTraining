9fa2b0940f01aa249ca0ff4c45ee289e6c959901
cezarr46 <sezay.ismail@gmail.com>
2017-07-17 17:52:41 +0300

    Add files via upload

---
+++ Module7/Degree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module7
+{
+    internal class Degree
+    {
+        private string degreeName;
+        private int creditsRequired;
+        private Course coursee;
+
+        public string DegreeName
+        {
+            get { return degreeName; }
+            set { degreeName = value; }
+        }
+
+        public int CreditsRequired
+        {
+            get { return creditsRequired; }
+            set { creditsRequired = value; }
+        }
+
+        public Course Coursee
+        {
+            get { return coursee; }
+            set { coursee = value; }
+        }
+
+        public Degree()
+        {
+
+        }
+
+        public Degree(string degreeName, Course coursee, int creditsRequired)
+        {
+            this.DegreeName = degreeName;
+            this.Coursee = coursee;
+            this.CreditsRequired = creditsRequired;
+        }
+    }
+}
