ea230e811098c7c6129c954b196ae3fbbfd1ceb4
cezarr46 <sezay.ismail@gmail.com>
2017-07-17 17:21:08 +0300

    Add files via upload

---
+++ Module5/Teacher.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module5
+{
+    public class Teacher
+    {
+        private string firstName;
+        private string lastName;
+        private string city;
+        private int zip;
+        private string country;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        public int Zip
+        {
+            get { return zip; }
+            set { zip = value; }
+        }
+
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+
+        public Teacher()
+        {
+          
+        }
+
+        public Teacher(string firstName, string lastName, string city, int zip, string country)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.City = city;
+            this.Zip = zip;
+            this.Country = country;
+        }
+    }
+}
