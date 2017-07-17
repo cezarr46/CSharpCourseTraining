2841d92efd7a095c2bef4af90b57d24e8d46e236
cezarr46 <sezay.ismail@gmail.com>
2017-07-17 17:57:12 +0300

    Add files via upload

---
+++ Module8/Course.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module8
+{
+    internal class Course
+    {
+        private string courseName;
+        private Degree degree;
+
+        public string CourseName
+        {
+            get { return courseName; }
+            set { courseName = value; }
+        }
+
+        public Degree Degreee
+        {
+            get { return degree; }
+            set { degree = value; }
+        }
+
+        public Course()
+        {
+
+        }
+
+        public Course(string courseName, List<Student> students, List<Teacher> teachers)
+        {
+            this.CourseName = courseName;
+        }
+    }
+}
